00fa1da1f29d37f32e3803f9d611e8a9ed585919
BaronFonMonc <sem.tima@yandex.ru>
2021-06-09 02:17:37 +0300

    renamed few files and added scripts

---
+++ src/lib.R
@@ -0,0 +1,15 @@
+
+setwd("D:/PuTTY_Project/hse21_H3K4me1_G4_human/src/")
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+DATA_DIR <- '../data/'
+OUT_DIR <- '../images/'
+
+prefix_1 <- 'H3K4me1_GM23338_ENCFF474FMV'
+prefix_2 <- 'H3K4me1_GM23338_ENCFF986GPP'
+
+
